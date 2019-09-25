202a81de93d148e8cad8cd39d479234d2ae401c7
JackHamby <jackhamby@JackHambys-MBP.lan>
2019-09-24 20:56:06 -0500

    updated types

--- dist/app.js
+++ dist/app.js
@@ -6,18 +6,47 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const http_1 = __importDefault(require("http"));
 const socket_io_1 = __importDefault(require("socket.io"));
-// import AppState from '../../multiplayer_battle_arena/src/components/application/application';
+function create_UUID() {
+    var dt = new Date().getTime();
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        var r = (dt + Math.random() * 16) % 16 | 0;
+        dt = Math.floor(dt / 16);
+        return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+    });
+    return uuid;
+}
 const app = express_1.default();
 const httpServer = http_1.default.createServer(app);
-const io = socket_io_1.default(httpServer);
+const io = socket_io_1.default(httpServer, {});
+let state = {
+    players: [],
+    error: null
+};
 io.listen(3001);
 io.on('connection', function (socket) {
-    console.log('a user asdasds');
-    console.log(AppState);
-    console.log('hot fuck');
+    console.log('a user was added');
+    const player = {
+        id: create_UUID(),
+        name: `player${state.players.length}`,
+        x: 0,
+        y: 0
+    };
+    const welcomeMessage = {
+        type: "welcome",
+        data: {
+            id: player.id,
+            state: state
+        }
+    };
+    state.players.push(player);
+    socket.send(welcomeMessage);
     socket.on('message', message => {
-        console.log(message);
-        socket.send({ "name": "assfuck" });
+        console.log(`message from client ${message}`);
+        // socket.send({"name" : "assfuck"});
     });
+    socket.on('disconnect', event => {
+        console.log('player disconnected');
+    });
+    console.log(state);
 });
 //# sourceMappingURL=app.js.map
--- src/app.tsx
+++ src/app.tsx
@@ -10,24 +10,78 @@ export interface AppState {
 }
 
 export interface Player {
-    ip: string;
+    id: string;
     name: string;
     x: number;
     y: number;
 }
 
+export interface Message {
+    type: string;
+    data: Object;
+}
+
+export interface WelcomeData {
+    id: string;
+    state: AppState;
+}
+
+
+function create_UUID(){
+    var dt = new Date().getTime();
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = (dt + Math.random()*16)%16 | 0;
+        dt = Math.floor(dt/16);
+        return (c=='x' ? r :(r&0x3|0x8)).toString(16);
+    });
+    return uuid;
+}
+
+
 const app = express();
 const httpServer = http.createServer(app);
-const io = socketIO(httpServer);
+const io = socketIO(httpServer, {});
 
-io.listen(3001)
+let state = {
+    players: [],
+    error: null
+
+} as AppState;
+
+io.listen(3001);
 
 io.on('connection', function(socket){
-    console.log('a user asdasds');
-    socket.on('message', message => {
-        console.log(message)
-        socket.send({"name" : "assfuck"})
+    console.log('a user was added')
+    
+    const player = {
+        id: create_UUID(),
+        name: `player${state.players.length}`,
+        x: 0,
+        y: 0
+    } as Player;
 
+    const welcomeMessage = {
+        type: "welcome",
+        data: {
+            id: player.id,
+            state: state
+        } as WelcomeData
+    }
+
+    state.players.push(player)
+
+    socket.send(welcomeMessage);
+
+    socket.on('message', message => {
+        console.log(`message from client ${message}`)
+        // socket.send({"name" : "assfuck"});
     })
+    socket.on('disconnect', event => {
+        console.log('player disconnected')
+    })
+
+
+
+    console.log(state)
 });
 
