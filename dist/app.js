4c9a975b3a07145c97439e1d63beafa36b21aed0
JackHamby <jackhamby@JackHambys-MacBook-Pro.local>
2019-09-29 15:49:41 -0500

    attempting deployment

--- dist/app.js
+++ dist/app.js
@@ -6,6 +6,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const http_1 = __importDefault(require("http"));
 const socket_io_1 = __importDefault(require("socket.io"));
+exports.WELCOME_MESSAGE = "welcome";
+exports.REMOVE_PLAYER_MESSAGE = "remove_player";
+exports.ADD_PLAYER_MESSAGE = "add_player";
+exports.DISCONNECT_MESSAGE = "disconnect";
+exports.UPDATE_PLAYER_MESSAGE = "update_player";
 function create_UUID() {
     var dt = new Date().getTime();
     var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
@@ -19,34 +24,115 @@ const app = express_1.default();
 const httpServer = http_1.default.createServer(app);
 const io = socket_io_1.default(httpServer, {});
 let state = {
-    players: [],
+    players: {},
     error: null
 };
 io.listen(3001);
-io.on('connection', function (socket) {
-    console.log('a user was added');
+const handleMessage = (message) => {
+    // console.log(JSON.stringify(message))
+    switch (message.type) {
+        case (exports.DISCONNECT_MESSAGE):
+            console.log(`disconnect player ${message.data.id}`);
+            // console.log(message.data.id)
+            removePlayer(message.data.id);
+            break;
+        case (exports.UPDATE_PLAYER_MESSAGE):
+            console.log(`update player ${message.data.id}`);
+            updatePlayer(message.data.id, message.data.x, message.data.y);
+            break;
+    }
+};
+const createPlayer = () => {
     const player = {
         id: create_UUID(),
         name: `player${state.players.length}`,
         x: 0,
         y: 0
     };
+    return player;
+};
+const updatePlayer = (id, x, y) => {
+    try {
+        state.players[id].x = x;
+        state.players[id].y = y;
+        console.log('\n');
+        console.log(`updated player ${id}`);
+        console.log('\n');
+        const updatePlayerMessage = {
+            type: exports.UPDATE_PLAYER_MESSAGE,
+            data: {
+                id,
+                x,
+                y
+            }
+        };
+        io.sockets.emit("message", updatePlayerMessage);
+        return true;
+    }
+    catch (error) {
+        console.log(error);
+        return false;
+    }
+};
+const addPlayer = () => {
+    try {
+        const player = createPlayer();
+        state.players[player.id] = player;
+        const addPlayerMessage = {
+            type: exports.ADD_PLAYER_MESSAGE,
+            data: {
+                id: player.id
+            }
+        };
+        io.sockets.emit("message", addPlayerMessage);
+        console.log('\n');
+        console.log(`added player ${player.id}`);
+        console.log('\n');
+        return player.id;
+    }
+    catch (error) {
+        console.log(error);
+        return "";
+    }
+};
+const removePlayer = (id) => {
+    try {
+        delete state.players[id];
+        const removePlayerMessage = {
+            type: exports.REMOVE_PLAYER_MESSAGE,
+            data: {
+                id: id
+            }
+        };
+        console.log('\n');
+        console.log(`removed player ${id}`);
+        console.log('\n');
+        io.sockets.emit("message", removePlayerMessage);
+        return true;
+    }
+    catch (error) {
+        console.log(error);
+        return false;
+    }
+};
+io.on('connection', function (socket) {
+    // console.log('a user was added');
+    const playerId = addPlayer();
     const welcomeMessage = {
         type: "welcome",
         data: {
-            id: player.id,
+            id: playerId,
             state: state
         }
     };
-    state.players.push(player);
     socket.send(welcomeMessage);
     socket.on('message', message => {
         console.log(`message from client ${message}`);
-        // socket.send({"name" : "assfuck"});
+        handleMessage(message);
     });
     socket.on('disconnect', event => {
         console.log('player disconnected');
+        console.log(event);
     });
-    console.log(state);
 });
 //# sourceMappingURL=app.js.map
--- src/app.tsx
+++ src/app.tsx
@@ -2,10 +2,13 @@
 import express from 'express';
 import http from 'http';
 import socketIO from 'socket.io';
+import { create } from 'domain';
 
 // temporary fix until i can import interface from another project
 export interface AppState {
-    players: Player[];
+    players: {
+        [key: string]: Player;
+    }
     error?: string;
 }
 
@@ -18,13 +21,43 @@ export interface Player {
 
 export interface Message {
     type: string;
-    data: Object;
+    data: any;
+}
+
+export const WELCOME_MESSAGE = "welcome";
+
+export const REMOVE_PLAYER_MESSAGE = "remove_player";
+
+export const ADD_PLAYER_MESSAGE = "add_player";
+
+export const DISCONNECT_MESSAGE = "disconnect";
+
+export const UPDATE_PLAYER_MESSAGE = "update_player"
+
+export interface UpdatePlayerData {
+    id: string;
+    x: number;
+    y: number;
 }
 
 export interface WelcomeData {
     id: string;
     state: AppState;
 }
+
+export interface DisconnectData {
+    id: string;
+}
+export interface RemovePlayerData {
+    id: string
+}
+
+export interface AddPlayerData {
+    id: string,
+    x: number,
+    y: number,
+}
+
 
 
 function create_UUID(){
@@ -43,45 +76,134 @@ const httpServer = http.createServer(app);
 const io = socketIO(httpServer, {});
 
 let state = {
-    players: [],
+    players: {},
     error: null
 
 } as AppState;
 
 io.listen(3001);
 
-io.on('connection', function(socket){
-    console.log('a user was added')
-    
+const handleMessage = (message: Message) => {
+    // console.log(JSON.stringify(message))
+    switch(message.type){
+        case (DISCONNECT_MESSAGE):
+            console.log(`disconnect player ${message.data.id}`);
+            // console.log(message.data.id)
+            removePlayer(message.data.id)
+            break;
+        case (UPDATE_PLAYER_MESSAGE):
+            console.log(`update player ${message.data.id}`);
+            updatePlayer(message.data.id, message.data.x, message.data.y);
+            break;
+    }
+
+}
+
+const createPlayer = (): Player => {
     const player = {
         id: create_UUID(),
         name: `player${state.players.length}`,
         x: 0,
         y: 0
     } as Player;
+    return player
 
+}
+
+const updatePlayer = (id: string, x: number, y: number): boolean=> {
+    try{
+        state.players[id].x = x;
+        state.players[id].y = y;
+        console.log('\n');
+        console.log(`updated player ${id}`);
+        console.log('\n');
+        const updatePlayerMessage = {
+            type: UPDATE_PLAYER_MESSAGE,
+            data: {
+                id,
+                x, 
+                y
+            } as UpdatePlayerData
+        } as Message
+        io.sockets.emit("message", updatePlayerMessage);
+        return true
+    }
+    catch (error){
+        console.log(error)
+        return false;
+    }
+}
+
+
+const addPlayer = (): string => {
+    try{
+        const player = createPlayer();
+        state.players[player.id] = player
+        const addPlayerMessage = {
+            type: ADD_PLAYER_MESSAGE,
+            data: {
+                id: player.id
+            } as AddPlayerData
+        } as Message
+        io.sockets.emit("message", addPlayerMessage);
+        console.log('\n');
+        console.log(`added player ${player.id}`);
+        console.log('\n');
+        return player.id;
+    }
+    catch(error){
+        console.log(error)
+        return "";
+    }
+}
+
+const removePlayer = (id: string): boolean => {
+    try {
+        delete state.players[id];
+        const removePlayerMessage = {
+            type: REMOVE_PLAYER_MESSAGE,
+            data: {
+                id: id
+            } as RemovePlayerData
+        }
+        console.log('\n');
+        console.log(`removed player ${id}`);
+        console.log('\n');
+
+        io.sockets.emit("message", removePlayerMessage);
+        return true;
+
+    }
+    catch (error){
+        console.log(error)
+        return false
+    }
+}
+
+
+io.on('connection', function(socket){
+    // console.log('a user was added');
+    
+    const playerId = addPlayer();
+    
     const welcomeMessage = {
         type: "welcome",
         data: {
-            id: player.id,
+            id: playerId,
             state: state
         } as WelcomeData
-    }
-
-    state.players.push(player)
+    } 
 
     socket.send(welcomeMessage);
 
     socket.on('message', message => {
         console.log(`message from client ${message}`)
-        // socket.send({"name" : "assfuck"});
+        handleMessage(message);
     })
+
     socket.on('disconnect', event => {
-        console.log('player disconnected')
+        console.log('player disconnected');
+        console.log(event)
     })
-
-
-
-    console.log(state)
 });
 
